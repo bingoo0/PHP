149c33eaa467c20e6b0af39315abdff8658bea22
bingoo0 <mehmedsezgin01@gmail.com>
2018-02-28 23:37:58 +0200

    add test route with view

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Php Course Project @yield('title')</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div style="padding:25px;background: lightblue;">Header</div>
+	
+		<div style="padding:25px;background: green;">
+
+			@section('menu')
+				<a href="#">Home</a>
+				<a href="#">Contacts</a>
+			@show
+		</div>
+
+		<div style="min-height: 700px;">
+			@yield('content')
+		</div>
+
+	<div style="padding:25px;background: lightblue;">Footer</div>
+	@stack('scripts')
+	@yield('scripts')
+</body>
+</html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title','Home')
+
+
+@section('content')
+
+<h1>Hello From Home Page</h1>
+
+@endsection
